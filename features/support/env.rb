75f8a5715e68b939f9bdd03427f44e7d4569ad11
Aditya Jain <ajjain92@gmail.com>
2016-07-28 16:53:05 +0530

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,7 +5,7 @@ require 'appium_capybara'
 
 DEVICE_ID = ENV['ADB_DEVICE_ARG']
 APPIUM_PORT = ENV['APPIUM_PORT']
-HIVE_QUEUE_NAME = "myhiveTest"
+HIVE_QUEUE_NAME = ENV['HIVE_QUEUE_NAME']
 def appium_caps
     {
         'MY_DEVICE'=> { platformName: "Android",deviceName: "#{HIVE_QUEUE_NAME}",udid: "#{DEVICE_ID}", browserName: "chrome"} 
@@ -16,7 +16,7 @@ end
 Capybara.register_driver :appium do |app|
     caps = appium_caps.fetch('MY_DEVICE')
     desired_caps = caps
-    url = "http://0.0.0.0:#{APPIUM_PORT}/wd/hub/" # Url to your running appium server
+    url = "http://127.0.0.1:#{APPIUM_PORT}/wd/hub/" # Url to your running appium server
     appium_lib_options = { server_url: url }
     all_options = { appium_lib:  appium_lib_options, caps: desired_caps }
     Capybara::Selenium::Driver.new(app, {:browser => :remote, :url => url, :desired_capabilities => caps})
